8a2b2a1e7f0c147a6edc44ca9da2572276ad53e1
vusaltagiev <vusal@hotmail.com>
2023-09-02 22:04:17 +0200

    vusal

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <p>Hello World!</p>
+    <p className=''>Hello World!</p>
   </React.StrictMode>
 );
 
