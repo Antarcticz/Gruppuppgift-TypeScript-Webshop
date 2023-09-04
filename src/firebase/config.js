0ec8225bf05fb239a06a0ca70abf36fba5dcfdf7
Maximilian <maximilian_hedman@hotmail.com>
2023-09-04 12:12:12 +0200

    added env file for firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DB,
+  projectId: process.env.REACT_APP_PID,
+  storageBucket: process.env.REACT_APP_SB,
+  messagingSenderId: process.env.REACT_APP_SID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MID
+};
+
+initializeApp(firebaseConfig);
+
+const db = getFirestore()
+const auth = getAuth()
+
+export { db, auth}
